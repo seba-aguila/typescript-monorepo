a229effd3fa91abaa82104af504efc29d56775d1
seba-aguila <allarines@gmail.com>
2026-05-02 17:34:03 -0400

    Don't clean directories on hot reload build.

--- packages/common/tsdown.config.ts
+++ packages/common/tsdown.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: "src/index.ts",
+  clean: false,
   format: ["esm"],
   target: "es2022",
   sourcemap: true,
   treeshake: true,
   dts: true,
-  exports: true,
+  exports: false,
   unbundle: true,
   logLevel: "error",
 });
